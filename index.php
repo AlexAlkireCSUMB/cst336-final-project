971ae0e13b6dcb718fa8d59895ed733de6ed4a04
Alex Alkire <aalkire@csumb.edu>
2018-12-06 10:27:13 +0000

    Layout setup

--- index.php
+++ index.php
@@ -2,41 +2,68 @@
 <html>
   <head>
     <link rel="icon" href="./img/steam.png">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  
 
     <title>Steam Linker</title>
     
-    <link href="./css/bootstrap.min.css" rel="stylesheet">
     <link href="./css/styles.css" rel="stylesheet">
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    
   </head>
 
-  <body>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li id="nav_home"  class="nav_element" class="active" ><a href="#" onClick='onClickHome()'>Home</a></li>
-            <li id="nav_about" class="nav_element"><a href="#about" onclick='onClickAbout()'>About</a></li>
-            <li id="nav_contact" class="nav_element"><a href="#contact" onclick='onClickContact()'>Contact</a></li>
-          </ul>
-        </div>
+<body>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        </button>
+      <p class="navbar-brand">Steam Linker</p>
+    </div>
+    <div id="navbar" class="collapse navbar-collapse">
+      <ul id="nav_list" class="nav navbar-nav"></ul>
+      
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+			<ul id="login-dp" class="dropdown-menu">
+				<li>
+					 <div class="row">
+							<div class="col-md-12">
+										<div class="form-group">
+											 <label for="emailInput">Email address</label>
+											 <input type="email" class="form-control" id="emailInput" placeholder="Email address">
+										</div>
+										<div class="form-group">
+											 <label for="passwordInput">Password</label>
+											 <input type="password" class="form-control" id="passwordInput" placeholder="Password">
+                      <div class="help-block text-right"><a href="">Forgot Password?</a></div>
+										</div>
+										  <button class="btn btn-primary btn-block" onclick="signIn()">Sign in</button>
+							</div>
+							<div class="bottom text-center">
+								New? <a href="#"><b>Register for an account</b></a>
+							</div>
+					 </div>
+				</li>
+			</ul>
+      
       </div>
     </nav>
-
-    <div class="container">
-
-
-    </div>
-    <script src="./js/ui.js"></script>
+      <div class="container fill">
+        <div class="row main_row">
+          <div id="sidebar" class="col-md-3"></div>
+        <div id="main_div" class="col-md-8">
+          <p id="main_text">foo</p>
+        </div>
+      </div>
+    </div> 
     
+    <script src="./js/ui.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="./js/bootstrap.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-latest.js"></script>
+    <script type="text/javascript" src="js/bootstrap-dropdown.js"></script>
   </body>
 </html>
--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,20 @@
 var activeNavBarElem = "nav_home"
+var aboutText ="Welcome to Steam Linker, a site that lets you compare games between Steam users by their Steam or Slack accounts."
 
+
+$(function() {
+  $('.dropdown-toggle').dropdown();
+  $('.dropdown input, .dropdown label').click(function(e) {
+    e.stopPropagation();
+  });
+});
+
+
+function signIn(){
+    var emailAddr = document.getElementById("emailInput").value;
+    var pw = document.getElementById("passwordInput").value;
+    alert("Sign in attempt.\nEmail: "+emailAddr+"\nPassword: "+pw);
+}
 
 function updateNavBar(elemStr){
     var oldElem = document.getElementById(activeNavBarElem);
@@ -10,15 +25,129 @@ function updateNavBar(elemStr){
     
 }
 
-function onClickAbout(){
-    updateNavBar("nav_about");
-    alert("About pressed");
+class NavBarElem {
+    constructor(name, id){
+        var navBarE = this
+        this.name = name;
+        this.id= id;
+        this.updatePage = function f(){
+        
+        };
+        this.onClick = function f() {
+            updateNavBar(id);
+            navBarE.updatePage();
+        };
+    }
 }
-function onClickHome(){
-    updateNavBar("nav_home");
-    alert("Home pressed");
+
+function clearMain(){
+    document.getElementById("main_text").innerHTML="";
 }
-function onClickContact(){
-    updateNavBar("nav_contact");
-    alert("Contact pressed");
+class NavBar {
+    constructor(){
+        
+        /* Configure navbar here */
+        this.navElems = {
+            home : new NavBarElem('Home', 'nav_home'),
+            about : new NavBarElem('About', 'nav_about'),
+            contact : new NavBarElem('Contact', 'nav_contact')
+        };
+        this.navElems['home'].updatePage = function(){
+            clearMain();
+        };
+        this.navElems['about'].updatePage = function(){
+            var mainText = document.getElementById("main_text");
+            mainText.innerHTML=aboutText;
+        };
+        this.navElems['contact'].updatePage = function(){
+            clearMain();            
+        };
+        /* Generate navbar */
+        var nl = document.getElementById("nav_list");
+        for(var key in this.navElems){
+            var e = this.navElems[key];
+            var listElem = document.createElement('li');
+            listElem.setAttribute('id', e.id);
+            listElem.classList.add("nav_element");
+            var aElem = document.createElement('a');
+            aElem.setAttribute('href', "#");
+            aElem.innerHTML=e.name;
+            listElem.onclick=e.onClick;
+            listElem.appendChild(aElem);
+            nl.appendChild(listElem);
+        }
+    }
 }
+var navBar = new NavBar();
+
+
+function displayUser(usr){
+    var sb = document.getElementById('sidebar')
+    sb.appendChild(usr.generateRowElement());
+}        
+
+class slUser {
+    constructor(name, id, imgName){
+        this.name = name;
+        this.id = id;
+        this.imgName = imgName;
+        this.elem = null;
+        this.remove = function f(){
+            alert("Removing "+name+" from list.");
+        };
+    }
+    
+    generateRowElement(){
+        if(this.elem != null){
+            return this.elem;   
+        }
+        var userRow = document.createElement('div');
+        userRow.classList.add("row");
+        userRow.classList.add("user_row");
+        var slIconCol = document.createElement('div');
+        slIconCol.classList.add("user_icon_col");
+        slIconCol.classList.add("col-sm-1");
+        var slackImage = document.createElement('img');
+        slackImage.classList.add("profile_image");
+        slackImage.setAttribute('src', 'img/slack.png');
+        var stIconCol = document.createElement('div');
+        stIconCol.classList.add("user_icon_col");
+        stIconCol.classList.add("col-sm-1");
+        var steamImage = document.createElement('img');
+        steamImage.classList.add("profile_image");
+        var steamImageLink = document.createElement('a');
+        steamImageLink.setAttribute('href', 'https://steamcommunity.com/id/'+this.id);
+        steamImage.setAttribute('src', 'img/steam.png');
+        steamImageLink.appendChild(steamImage);
+        var userNameCol = document.createElement('div');
+        userNameCol.classList.add("col-sm-4");
+        userNameCol.classList.add("user_name_col");
+        var userNameLink = document.createElement('p');
+        userNameLink.classList.add("username_text");
+        userNameLink.innerHTML = this.name;
+        
+        var rmColumn = document.createElement('div');
+        rmColumn.classList.add("col-sm-1");
+        //rmColumn.
+        //var rmLink = document.createElement('a');
+        var rmImg = document.createElement('input');
+        rmImg.setAttribute('type','image');
+        rmImg.setAttribute('src', 'img/cross.png');
+        rmImg.classList.add("rm_img");
+        rmImg.onclick = this.remove;  
+        userRow.appendChild(stIconCol);
+        userRow.appendChild(slIconCol);
+        userRow.appendChild(userNameCol);
+        userNameCol.appendChild(userNameLink);
+        slIconCol.appendChild(slackImage);
+        stIconCol.appendChild(steamImageLink);
+        rmColumn.appendChild(rmImg);
+        userRow.appendChild(rmColumn);
+        return userRow;
+    }
+}
+
+var usrA = new slUser('alex', 'ryuuarashi', 'steam.png');
+var usrB = new slUser('foobar', 'ryuuarashi', 'steam.png');
+displayUser(usrA);
+displayUser(usrB);
